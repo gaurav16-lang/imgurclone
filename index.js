4cc2dad50adb33a64f4b9cfb19af1f71672b6e77
Gaurav Maihuria <maihuriagaurav16@gmail.com>
2022-02-01 12:27:35 +0530

    debouncing completed

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let drop = document.getElementById('dropdown')
+var timerid
 const display = () => {
   fetch('https://api.imgflip.com/get_memes')
     .then((d) => d.json())
@@ -19,5 +21,73 @@ const showdata = (res) => {
 
     childs.append(img)
     parent.append(childs)
+  })
+}
+
+async function searchMovies(movie_name) {
+  try {
+    let res = await fetch(
+      `http://www.omdbapi.com/?apikey=d51aa006&s=${movie_name}`,
+    )
+    let data = await res.json()
+    return data
+  } catch (e) {
+    console.log('e', e)
+  }
+}
+async function main() {
+  let name = document.getElementById('searchbox').value
+
+  if (name.length === 0) {
+    drop.style.display = 'none'
+  }
+  if (name.length < 3) {
+    return false
+  }
+
+  let res = await searchMovies(name)
+  let moviesdata = res.Search
+  appendMovies(moviesdata)
+  console.log('res', moviesdata)
+}
+function debounce(func, delay) {
+  // lets talk about 4
+  //func =main()
+
+  drop.style.display = 'block'
+
+  if (timerid) {
+    clearTimeout(timerid)
+  }
+  timerid = setTimeout(function () {
+    func()
+  }, delay)
+}
+function appendMovies(movies) {
+  if (movies === undefined) {
+    return false
+  }
+  drop.innerHTML = null
+  movies.forEach(function (movie) {
+    let bigdiv = document.createElement('div')
+    bigdiv.setAttribute('class', 'bigdiv')
+    bigdiv.addEventListener('click', function () {
+      selectmovie(movie)
+    })
+    let div = document.createElement('div')
+    div.setAttribute('class', 'dropdiv')
+    let image = document.createElement('img')
+    image.src = movie.Poster
+    image.setAttribute('class', 'dropimage')
+    div.append(image)
+    let div2 = document.createElement('div')
+    div2.setAttribute('class', 'rightdiv')
+
+    let p = document.createElement('p')
+    p.setAttribute('class', 'dropdownp')
+    p.innerHTML = movie.Title
+    div2.append(p)
+    bigdiv.append(div, div2)
+    drop.append(bigdiv)
   })
 }
